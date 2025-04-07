4897de410590204309a30fb084d0b6dd0db4fc7c
erginbilgin0 <erginbilgin0@gmail.com>
2025-04-07 09:45:09 +0300

    ECOM2 first steps

---
+++ my-app/src/components/layout/header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () =>{
+    return <div className="pb-20 gap-16 sm:mx-20 bg-red-600 h-10 items-center justify-items-center">HEADER</div>
+}
+
+export default Header;
