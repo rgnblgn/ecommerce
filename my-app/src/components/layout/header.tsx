8644dad44deb4175609d0623953ec736131b13a6
erginbilgin0 <erginbilgin0@gmail.com>
2025-04-29 14:30:57 +0300

    ECOM3 header update

--- my-app/src/components/layout/header.tsx
+++ my-app/src/components/layout/header.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
+import AccountMenu from './accountMenu.tsx';
 
 const Header = () =>{
-    return <div className="pb-20 gap-16 sm:mx-20 bg-red-600 h-10 items-center justify-items-center">HEADER</div>
+
+    const [isVisible,setIsVisible] = useState(false)
+
+    const openMyAccount = ()=>{
+        setIsVisible(!isVisible)
+    }
+
+    return  <div className="sticky flex py-[20px] px-[10px] gap-16 sm:mx-20 bg-red-600 h-20 items-center justify-between">
+        <div className="text-blue-600 font-bold cursor-pointer pl-[10px]">TakeIt</div>
+        <div className="block">
+            <div onClick={openMyAccount} className="text-blue-600 font-bold cursor-pointer pr-[10px]">MyAccount</div>   
+            {isVisible && <div className=" absolute w-[200px] h-[500px] bg-blue-100 right-[80px]">
+                <AccountMenu></AccountMenu>
+                </div>}
+        </div>
+    </div>
+
 }
 
 export default Header;
--- my-app/src/pages/homepage/index.tsx
+++ my-app/src/pages/homepage/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Homepage = (props:any) =>{
-    return <div>Homepage</div>
+
+    return <div className="w-[100%] mt-[900px]">
+        <div className="bg-pink-600 w-[100%] h-[500px] ">CaroselBanner</div>
+        <div className="bg-yellow-600 w-[100%] h-[300px] mt-[100px]">Horizontal Product List</div>
+
+        </div>
 }
 
 export default Homepage;
